29056d87c0bf63ced06bd09ac65ddd5a54c47a0c
KiratGrewal <kaurg52@student.douglascollege.ca>
2019-11-16 22:17:00 -0800

    second class file

---
+++ ClassicCafeDB/src/Item.java
@@ -0,0 +1,20 @@
+
+public class Item {
+
+	private Menu item;
+	private int quantity;
+	public Menu getItem() {
+		return item;
+	}
+	public void setItem(Menu item) {
+		this.item = item;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	
+
+}
